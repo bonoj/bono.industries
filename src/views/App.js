965cd9f2a27baeddfbd8d386a741eb906006fec5
John Bono <johnbono@John.B-MBPro>
2021-07-28 23:01:39 -0700

    first commit

--- src/views/App.js
+++ src/views/App.js
@@ -1,23 +1,20 @@
-import logo from "../img/logo.svg";
+import React, {useState, useContext} from 'react'
+// import {Context} from "../store/appContext";
+// import logo from "../img/logo.svg";
 import "../style/App.css";
 
+
 function App() {
+  // const {store, actions} = useContext(Context)
+  // const [todo, setTodo]=useState("")
+  // const [todoList, setTodoList] =useState([])
+  // console.log("store", store.todoList)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input onChange={(e)=>setTodo(e.target.value)}/>
+      <button onClick={()=> actions.addTodo(todo)}>Add</button>
+      {store.todoList.map((e, index)=><TodoList todo={e} key={index}/>)} */}
+      <h3>Engineering a better tomorrow today.</h3>
     </div>
   );
 }
