a610f14d049a99fc6e30c0b9bbb05db35b9dca4a
John Bono <johnbono@John.B-MBPro>
2021-10-20 14:40:30 -0400

    Condense

--- src/views/App.js
+++ src/views/App.js
@@ -14,21 +14,21 @@ function App() {
       <button onClick={()=> actions.addTodo(todo)}>Add</button>
       {store.todoList.map((e, index)=><TodoList todo={e} key={index}/>)} */}
       <h3>Engineering a better tomorrow yesterday.</h3>
-      <a href="https://twitter.com/johnlbono" target="_blank" rel="noopener noreferrer">A little bird told me</a>
+      {/* <a href="https://twitter.com/johnlbono" target="_blank" rel="noopener noreferrer">A little bird told me</a>
       <br/>
       <a href="https://www.youtube.com/channel/UCnFN3vwlQABjNk26qKy3e2w?sub_confirmation=1" target="_blank" rel="noopener noreferrer">The bastard prince of Arioch</a>
-      <br/>
-      <a href="https://www.instagram.com/awolfandherboy/" target="_blank" rel="noopener noreferrer">And a wolf and her boy</a>
-      <br/>
-      <a href="https://docs.google.com/document/d/1R3b6HIWKKB0fBjnERarfM2JepEGoAXUCi11tCnC6fC8/" target="_blank" rel="noopener noreferrer">And Quoth the Raven</a>
+      <br/> */}
+      <a href="https://www.instagram.com/awolfandherboy/" target="_blank" rel="noopener noreferrer">A wolf and her boy</a>
+      {/* <br/>
+      <a href="https://docs.google.com/document/d/1R3b6HIWKKB0fBjnERarfM2JepEGoAXUCi11tCnC6fC8/" target="_blank" rel="noopener noreferrer">And Quoth the Raven</a> */}
       <br/>
       <a href="https://docs.google.com/document/d/1c2elXayXBWY1O4a3FfiE-P0wJWbbl_RmeOkKYPMWqY0/" target="_blank" rel="noopener noreferrer">Have come to play a game</a>
       <br/>
-      <a href="https://www.linkedin.com/in/bonoj/" target="_blank" rel="noopener noreferrer">At ye olde Linked Inn</a>
+      <a href="https://philome.la/Ironed_Man/towers/play/index.html" target="_blank" rel="noopener noreferrer">At the tops of towers</a>
       <br/>
-      <a href="https://philome.la/Ironed_Man/towers/play/index.html" target="_blank" rel="noopener noreferrer">And at the tops of clocktowers</a>
-      <br/>
-      <a href="https://vm.tiktok.com/ZMR442NfE/" target="_blank" rel="noopener noreferrer">Tick talk, tick talk</a>
+      <a href="https://www.linkedin.com/in/bonoj/" target="_blank" rel="noopener noreferrer">Find us</a>
+      {/* <br/>
+      <a href="https://vm.tiktok.com/ZMR442NfE/" target="_blank" rel="noopener noreferrer">Tick talk, tick talk</a> */}
       <br/>
       <br/>
       {/* <h3>Where in the world is the wandering bard?</h3>
